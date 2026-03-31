b58efdea7648dff8f6a28482cc1c259c5051ba43
Khadija Jumani <khadijajumani0208@gmail.com>
2026-03-31 20:53:40 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  turbopack: {
-    root: __dirname,
-  },
+
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
